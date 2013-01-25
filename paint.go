f2dd4b9f56d4ac59dd2c1f0a7c2e34d50527e2df
Terry.Mao <83873308@qq.com>
2013-01-25 18:19:46 +0800

    just use Resize for creating Thumbnail

--- paint.go
+++ paint.go
@@ -1,24 +1,10 @@
 package paint
 
 import (
-	"math"
 	"github.com/Terry-Mao/paint/magickwand"
 )
 
 func Thumbnail(wand *magickwand.MagickWand, width, height uint) error {
-	srcWidth := wand.Width()
-	srcHeight := wand.Height()
-	ratio := math.Min(float64(srcWidth)/float64(width),
-		float64(srcHeight)/float64(height))
-	scaleWidth := uint(float64(srcWidth) / ratio)
-	scaleHeight := uint(float64(srcHeight) / ratio)
-
 	// Resize
-	if err := wand.Resize(scaleWidth, scaleHeight, magickwand.GaussianFilter,
-		1.0); err != nil {
-		return err
-	}
-
-	// Crop
-	return wand.Crop(width, height, 0, 0)
+	return wand.Resize(width, height, magickwand.GaussianFilter, 1.0)
 }
